2b43c678d45d5b418de63a85159f6deb0f1600b2
Rencas <renzo.c@ohecommerce.agency>
2023-04-15 22:59:07 -0500

    quiosco-app getCategorys y request database

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+export default function Home({ categorys }) {
+  console.log(categorys);
   return <h1>Next.js</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categorys = await prisma.category.findMany();
+
+  return {
+    props: {
+      categorys,
+    },
+  };
+};
