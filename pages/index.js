0730b6a082c953e9bf397a15b3051d11928fbc6a
Rencas <renzo.c@ohecommerce.agency>
2023-04-15 23:29:13 -0500

    quiosco-app create components + layout

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import Layout from '@/layout/Layout';
 
-export default function Home({ categorys }) {
-  console.log(categorys);
-  return <h1>Next.js</h1>;
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
+  );
 }
-
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient();
-
-  const categorys = await prisma.category.findMany();
-
-  return {
-    props: {
-      categorys,
-    },
-  };
-};
