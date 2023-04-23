1d32adfc117ea56c2ea40fd89999c790b0a3b6b1
Rencas <renzo.c@ohecommerce.agency>
2023-04-23 01:27:15 -0500

    quiosco-app sending + insert registers in the bd

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,7 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
 export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+
   if (req.method === 'POST') {
-    res.json({ method: 'POST!!' });
-  } else {
-    res.json({ method: 'GET' });
+    const order = await prisma.orden.create({
+      data: {
+        name: req.body.name,
+        total: req.body.total,
+        pedido: req.body.pedido,
+        fecha: req.body.fecha,
+      },
+    });
+
+    res.json(order);
   }
 }
