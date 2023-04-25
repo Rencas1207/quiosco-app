fe4adb7af5a2de419122005a7068c3eb1a9bae40
Rencas <renzo.c@ohecommerce.agency>
2023-04-25 09:47:00 -0500

    quiosco-app show the rest of the order information

--- components/Order.js
+++ components/Order.js
@@ -1,9 +1,41 @@
-const Order = ({ order }) => {
-  const { id, name, total, pedido } = order;
+import { formatMoney } from '@/helpers';
+import Image from 'next/image';
+
+const Order = ({ orden }) => {
+  const { id, name, total, order } = orden;
   return (
     <div className="border p-10 space-y-5">
       <h3 className="text-2xl font-bold">Orden: {id}</h3>
       <p className="text-lg font-bold">Cliente: {name}</p>
+
+      <div>
+        {order?.map((platillo) => (
+          <div
+            key={platillo.id}
+            className="py-3 flex border-b last-of-type:border-0 items-center"
+          >
+            <div className="w-32">
+              <Image
+                width={300}
+                height={400}
+                src={`/assets/img/${platillo.image}.jpg`}
+                alt={`Imagen platillo ${platillo.name}`}
+              />
+            </div>
+            <div className="p-5 space-y-2">
+              <h4 className="text-xl font-bold text-amber-500">
+                {platillo.name}
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="md:flex md:items-center md:justify-between my-10">
+        <p className="mt-5 font-black text-4xl text-amber-500">
+          Total a pagar: {formatMoney(total)}
+        </p>
+      </div>
     </div>
   );
 };
